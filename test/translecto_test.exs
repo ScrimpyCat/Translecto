cb26181d2cc9012f99eb0fe994893756873b587f
Stefan Johnson <sj@scrimpycat.io>
2016-09-04 05:34:06 +1000

    Test schema and queries

--- test/translecto_test.exs
+++ test/translecto_test.exs
@@ -1,8 +1,107 @@
 defmodule TranslectoTest do
-  use ExUnit.Case
-  doctest Translecto
+    use ExUnit.Case
+    use Translecto.Query
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+    defmodule Model do
+        defmodule Locale do
+            use Ecto.Schema
+
+            schema "locales" do
+                field :country, :string
+                field :language, :string
+            end
+        end
+
+        defmodule IngredientNameTranslation do
+            use Ecto.Schema
+            use Translecto.Schema.Translation
+
+            schema "ingredient_name_translations" do
+                translation
+                field :term, :string
+            end
+        end
+
+        defmodule IngredientTypeTranslation do
+            use Ecto.Schema
+            use Translecto.Schema.Translation
+
+            schema "ingredient_type_translations" do
+                translation
+                field :term, :string
+            end
+        end
+
+        defmodule Ingredient do
+            use Ecto.Schema
+            use Translecto.Schema.Translatable
+
+            schema "ingredients" do
+                translatable :name, IngredientNameTranslation
+                translatable :type, IngredientTypeTranslation
+            end
+        end
+
+    end
+
+    test "get_translation" do
+        assert Model.Ingredient.get_translation(:name) == Model.IngredientNameTranslation
+        assert Model.Ingredient.get_translation(:type) == Model.IngredientTypeTranslation
+    end
+
+    test "translate name" do
+        query = from i in Model.Ingredient,
+            locale: 1,
+            translate: name in i.name
+
+        result = from i in Model.Ingredient,
+            join: name in Model.IngredientNameTranslation,
+            where: i.name == name.translate_id and name.locale_id == 1
+
+        assert inspect(query) == inspect(result)
+    end
+
+    test "translate type" do
+        query = from i in Model.Ingredient,
+            locale: 1,
+            translate: type in i.type
+
+        result = from i in Model.Ingredient,
+            join: type in Model.IngredientTypeTranslation,
+            where: i.type == type.translate_id and type.locale_id == 1
+
+        assert inspect(query) == inspect(result)
+    end
+
+    test "translate name and type" do
+        query = from i in Model.Ingredient,
+            locale: 1,
+            translate: name in i.name,
+            translate: type in i.type
+
+        result = from i in Model.Ingredient,
+            join: name in Model.IngredientNameTranslation,
+            where: i.name == name.translate_id and name.locale_id == 1,
+            join: type in Model.IngredientTypeTranslation,
+            where: i.type == type.translate_id and type.locale_id == 1
+
+        assert inspect(query) == inspect(result)
+    end
+
+    test "translate name and type with referencing translations" do
+        query = from i in Model.Ingredient,
+            locale: 1,
+            translate: name in i.name,
+            translate: type in i.type,
+            select: { name.term, type.term }
+
+        result = from i in Model.Ingredient,
+            join: name in Model.IngredientNameTranslation,
+            where: i.name == name.translate_id and name.locale_id == 1,
+            join: type in Model.IngredientTypeTranslation,
+            where: i.type == type.translate_id and type.locale_id == 1,
+            select: { name.term, type.term }
+
+        assert inspect(query) == inspect(result)
+    end
 end
